12b0d3670a1af0f3d3c60c714912829aa9045a74
UnicornL-NgocLan <lengoclan8a22002@gmail.com>
2025-02-11 13:53:29 +0700

    remove expireIn

--- server/utils/jwtAttachmentAndCreation.js
+++ server/utils/jwtAttachmentAndCreation.js
@@ -1,18 +1,16 @@
 import jwt from 'jsonwebtoken'
 
 export const createJWT = ({ payload }) => {
-  const token = jwt.sign(payload, process.env.JWT_SECRET,{expiresIn:'1h'});
+  const token = jwt.sign(payload, process.env.JWT_SECRET);
   return token;
 };
 
 export const attachCookiesToResponse = ({ res, data }) => {
   const accessTokenJWT = createJWT({ payload: data });
 
-  const thirtyDays = 1000 * 60 * 60 * 24 * 30;
   res.cookie('stto', accessTokenJWT, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    expires: new Date(Date.now() + thirtyDays),
     sign: true
   });
 };
